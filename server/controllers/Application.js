b363aec9887afa3bf4f57c4095c15557cc3caa2e
Lokendra Mahale <lokendramahale@gmail.com>
2024-11-11 02:44:04 +0530

    dfsg

--- server/controllers/Application.js
+++ server/controllers/Application.js
@@ -15,10 +15,10 @@ exports.getAllAplication= async (req, res) => {
 exports.getAllInterviewSchedule = async (req, res) => {
     const { application_id, interview_date, interview_time } = req.body;
     let connection;
-    try {
+    try {        
         connection = await connect();
         const [results]=await connection.execute("SELECT ps.rounds,ps.date, c.company_name, us.name as SPOC ,js.job_title FROM placement_schedules AS ps  JOIN companies AS c ON ps.company_id = c.company_id JOIN users AS us ON us.user_id = ps.coordinator_id JOIN job_postings AS js ON js.company_id = ps.company_id;");
-        await connection.end();
+        await connection.end(); 
         res.status(200).json({ success: true, data:results,message: "FEtch all data success"});
     } catch (error) {
         res.status(500).json({ success: false, message: 'Error scheduling interview', error });
